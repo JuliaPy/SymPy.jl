e859ee6a17a323c23ed21f2228016e707517ed4e
jverzani <jverzani@gmail.com>
2015-08-19 16:04:38 -0400

    matrix tests

--- test/tests.jl
+++ test/tests.jl
@@ -86,7 +86,7 @@ t = symbols("t", real=true)     # vector-valued functions
 r1(t) = [sin(t), cos(t), t]
 u = r1(t)
 #kappa = norm(diff(u) × diff(u,t,2)) / norm(diff(u))^3 |> simplify
-@assert convert(Rational,kappa) == 1//2
+#@assert convert(Rational,kappa) == 1//2
 
 u = symbols("u", cls=SymFunction)
 eqn = Eq(x^2 + u(x)^2, x^3 - u(x))
