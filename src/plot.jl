JuliaPy/SymPy.jl

commit d09749931350a5f1c21cd70eef86f6b96bba7609

more fix with plots

src/plot.jl
@@ -292,21 +292,35 @@ Plots.createKWargsList(plt::Plots.PlottingObject, fx::Sym, fy::Sym, umin::Real, 
 
 ## Helper functions
 
-## the pattern Float64[ex(x) for x in xs] has the disadvantage of several round trips between Julia and Python
-## This functions passes the xs all at once to Python, has it loop there, and then returns the values. They
+## the fallback pattern Float64[ex(x) for x in xs] has the
+## disadvantage of several round trips between Julia and Python (and
+## being v0.3 incompatible.  This functions tries to pass the xs all at once
+## to Python, has it loop there, and then return the values. They
 ## come back as an array of type Any, so need conversion.
 function mapsubs(ex::Sym, x::Sym, vals::AbstractVector)
-    out = pyeval("[fn(x,val) for val in vals]", fn=project(ex)[:subs], x=project(x), vals=vals)
-    map(Float64, out)
+    out = Float64[]
+    try
+        out = pyeval("[fn(x,val) for val in vals]", fn=project(ex)[:subs], x=project(x), vals=vals)
+        out = map(Float64, out)
+    catch err
+        out = Float64[ex(x) for x in xs]
+    end
+    out
 end
 
 ## This is similar to the above, but is used for two variables. It
 ## mimics Float64[ex(x,y) for x in xs, y in ys]
 function mapsubs2(ex::Sym, x,xs, y, ys)
-    out = PyCall.pyeval("[fn(x,xval).subs(y,yval) for yval in yvals for xval in xvals]",
-                        fn=ex.x[:subs],x=x.x,y=y.x,xvals=xs, yvals=ys)
-    out = reshape(out, (length(xs), length(ys)))
-    map(Float64, out)
+    out = Float64[]
+    try
+        out = PyCall.pyeval("[fn(x,xval).subs(y,yval) for yval in yvals for xval in xvals]",
+                            fn=ex.x[:subs],x=x.x,y=y.x,xvals=xs, yvals=ys)
+        out = reshape(out, (length(xs), length(ys)))
+        out = map(Float64, out)
+    catch err
+        out =Float64[ex(x,y) for x in xs, y in ys]
+    end
+    out
 end
 
 

commit 501edabf7c92e3a49f3d00e639cfbc635838e9ff

Fixed bug introduced by d097499

src/plot.jl
@@ -303,7 +303,7 @@ function mapsubs(ex::Sym, x::Sym, vals::AbstractVector)
         out = pyeval("[fn(x,val) for val in vals]", fn=project(ex)[:subs], x=project(x), vals=vals)
         out = map(Float64, out)
     catch err
-        out = Float64[ex(x) for x in xs]
+        out = Float64[ex(x) for x in vals]
     end
     out
 end