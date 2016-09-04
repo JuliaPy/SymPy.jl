68f83d3ded896445d166d0a61cfcb80fbf262a95
jverzani <jverzani@gmail.com>
2016-09-04 14:33:04 -0400

    tidy

--- src/mathops.jl
+++ src/mathops.jl
@@ -5,7 +5,8 @@
 -{T<:SymbolicObject, S<:SymbolicObject}(x::T, y::S) = x + (-y)
 -(x::SymbolicObject)                    =  (-1) * x 
 /(x::Sym, y::Sym) = x * pycall(sympy[:Pow], Sym, y, -1)::Sym 
-^(x::SymbolicObject, y::Union{Integer, Rational}) = x^convert(Sym,y)
+^(x::SymbolicObject, y::Rational) = x^convert(Sym,y)
+^(x::SymbolicObject, y::Integer) = x^convert(Sym,y) # no Union{Integer, Rational}, as that has ambiguity
 ^(x::Sym, y::Sym) = pycall(sympy[:Pow], Sym, x, y)::Sym      
 //(x::SymbolicObject, y::Int) = x / Sym(y)
 //(x::SymbolicObject, y::Rational) = x / Sym(y)
