1293d63074c464aee0266f7fe9cca90eecb09e55
jverzani <jverzani@gmail.com>
2016-09-04 14:31:40 -0400

    tidy

--- src/mathops.jl
+++ src/mathops.jl
@@ -4,15 +4,14 @@
 *{T<:SymbolicObject, S<:SymbolicObject}(x::T, y::S) = pycall(sympy[:Mul], Sym, x, y)::T
 -{T<:SymbolicObject, S<:SymbolicObject}(x::T, y::S) = x + (-y)
 -(x::SymbolicObject)                    =  (-1) * x 
-/(x::Sym, y::Sym) = x * pycall(sympy[:Pow], Sym, y, -1)::Sym ## not SymbolicObject. Not sure why.
-^(x::SymbolicObject, y::Rational) = x^convert(Sym,y)
-^(x::SymbolicObject, y::Integer) = x^convert(Sym, y)
-^(x::Sym, y::Sym) = pycall(sympy[:Pow], Sym, x, y)::Sym      ## not SymbolicObject. Not sure why.
+/(x::Sym, y::Sym) = x * pycall(sympy[:Pow], Sym, y, -1)::Sym 
+^(x::SymbolicObject, y::Union{Integer, Rational}) = x^convert(Sym,y)
+^(x::Sym, y::Sym) = pycall(sympy[:Pow], Sym, x, y)::Sym      
 //(x::SymbolicObject, y::Int) = x / Sym(y)
 //(x::SymbolicObject, y::Rational) = x / Sym(y)
 //(x::SymbolicObject, y::SymbolicObject) = x / y
 
-\(x::SymbolicObject, y::SymbolicObject) = (y'/x')'
+\(x::SymbolicObject, y::SymbolicObject) = (y'/x')' # ?
 
 
 inv(x::SymbolicObject) = x\one(x)
