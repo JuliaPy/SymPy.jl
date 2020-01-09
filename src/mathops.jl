a9dbe84bcf990657b35917338c3ff89f675c8c84
john verzani <jverzani@gmail.com>
2020-01-09 10:50:57 -0500

    Issue 315 [close #315] (#316)
    * alternate definitions for mathematical operations, as brought up in #312

--- src/mathops.jl
+++ src/mathops.jl
@@ -4,12 +4,13 @@
 ## evaluate binary operations of symbolic objects
 ## XXX -- this may prove too narryw with the use of ::Sym
 
-+(x::SymbolicObject, y::SymbolicObject) = pycall(sympy.Add, Sym, x, y)
-*(x::SymbolicObject, y::SymbolicObject) = pycall(sympy.Mul, Sym, x, y)
++(x::SymbolicObject, y::SymbolicObject) = x.__add__(y)
+*(x::SymbolicObject, y::SymbolicObject) = x.__mul__(y)
+
 -(x::SymbolicObject, y::SymbolicObject) = x + (-y)
 -(x::SymbolicObject)                    =  (-1) * x
-/(x::SymbolicObject, y::SymbolicObject) = x * inv(y)
-^(x::SymbolicObject, y::SymbolicObject) = pycall(sympy.Pow, Sym, x, y)::Sym
+/(x::SymbolicObject, y::SymbolicObject) = x.__div__(y)
+^(x::SymbolicObject, y::SymbolicObject) = x.__pow__(y)
 ^(x::SymbolicObject, y::Rational) = x^convert(Sym,y)
 #^(x::SymbolicObject, y::Integer) = x^convert(Sym,y) # no Union{Integer, Rational}, as that has ambiguity
 //(x::SymbolicObject, y::Int) = x / Sym(y)
@@ -18,7 +19,4 @@
 
 \(x::SymbolicObject, y::SymbolicObject) = (y'/x')' # ?
 
-
-#inv(x::Sym) = x\one(x)
-#inv(x::Sym) = x^(-1)
-inv(x::Sym) = pycall(sympy.Pow, Sym, x, -1)
+inv(x::Sym) = x.__pow__(Sym(-1))
