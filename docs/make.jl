529e9fa99447f6dfa1c62d93d589e8e8a21bd471
jverzani <jverzani@gmail.com>
2023-09-25 10:32:14 -0400

    tweak doc generation. Something is broken

--- docs/make.jl
+++ docs/make.jl
@@ -30,6 +30,8 @@ pages = [
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+#=
+deploydocs(
+    repo = ""
+)
+=#
