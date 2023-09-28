473447b78b41a6359215789d47e0c5e44b8e2025
john verzani <jverzani@gmail.com>
2023-09-28 10:28:32 -0400

    Issue 528 (#529)
    * tweak lambdify for constants
    
    * adjust
    
    * rework
    
    * deploy docs through gh-pages, not juliahub

--- docs/make.jl
+++ docs/make.jl
@@ -30,8 +30,6 @@ pages = [
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=
 deploydocs(
-    repo = ""
+    repo = "github.com/JuliaPy/SymPy.jl/"
 )
-=#
