8884fcd27246c8bfa208bab9266e74303b98b80b
jverzani <jverzani@gmail.com>
2023-11-21 14:41:36 -0500

    add benchmark code

---
+++ benchmark/benchmarks.jl
@@ -0,0 +1,29 @@
+using BenchmarkTools
+using SymPy
+
+const SUITE = BenchmarkGroup()
+
+SUITE["creation"] = BenchmarkGroup(["sympy", "creation"])
+SUITE["expand"] = BenchmarkGroup(["sympy", "expand"])
+
+SUITE["creation"]["symbols","single"] = @benchmarkable symbols("x")
+SUITE["creation"]["symbols","multiple"] = @benchmarkable symbols("x,y,z")
+SUITE["creation"]["symbols","assumptions"] = @benchmarkable symbols("x", real=true)
+SUITE["creation"]["Sym","single"] = @benchmarkable Sym("x")
+SUITE["creation"]["Sym","multiple"] = @benchmarkable Sym("x,y,z")
+
+SUITE["creation"]["@syms","single"] = @benchmarkable @syms
+SUITE["creation"]["@syms","multiple"] = @benchmarkable @syms x, y, z
+SUITE["creation"]["@syms","assumptions"] = @benchmarkable @syms x::real
+
+SUITE["creation"]["Sym","number"] = @benchmarkable Sym(2)
+SUITE["creation"]["Sym","boolean"] = @benchmarkable Sym(true)
+
+
+@syms x::real
+SUITE["expand"]["expression","call"] = @benchmarkable sin(x)
+SUITE["expand"]["expression","sympy call"] = @benchmarkable sympy.expand_trig(sin(2x))
+SUITE["expand"]["expression","promotion"] = @benchmarkable x^2 - x + inv(x) - 1/x^2
+
+SUITE["expand"]["n",5] = @benchmarkable expand((x-a)^5)
+SUITE["expand"]["n",50] = @benchmarkable expand((x-a)^50)
